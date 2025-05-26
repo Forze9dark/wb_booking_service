e9c627ab7c826b62843778e488461bd8404f996e
Carlos Sanchez <78060927+Forze9dark@users.noreply.github.com>
2025-05-26 14:30:17 -0400

    Corregir error de sintaxis en template

--- public/templates/wp-booking-template.php
+++ public/templates/wp-booking-template.php
@@ -14,15 +14,15 @@
  */
 
 // Si este archivo es llamado directamente, abortar.
-if (!defined(\'WPINC\')) {
+if (!defined('WPINC')) {
     die;
 }
 
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?>> 
 <head>
-    <meta charset="<?php bloginfo(\'charset\'); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
     <?php wp_head(); // Esencial para que funcionen los scripts y estilos del plugin y WP ?>
